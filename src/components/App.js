d4302f8fef2b580747480ed3c4697bc3f2c1c17c
yaratavares <yaracriistavares@gmail.com>
2022-01-05 10:20:04 -0300

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+export default function App() {
+  return (
+      <BrowserRouter>
+        <Routes>
+            <Route path="/" element={} />
+            <Route path="/cadastro" element={}/>
+            <Route path="/habitos" element={} />
+            <Route path="hoje" element={}/>
+            <Route path="historico" element={} />
+        </Routes>
+      </BrowserRouter>
+  )
+}
