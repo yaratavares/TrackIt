9ea11515f450e689c8d0b0e99c380fb57b09a442
yaratavares <yaracriistavares@gmail.com>
2022-01-11 18:36:00 -0300

    bug no deploy

--- src/components/PageHistory/BlockCalendar/index.js
+++ src/components/PageHistory/BlockCalendar/index.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react/cjs/react.development";
+import { useEffect, useState } from "react";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 import dayjs from "dayjs";
